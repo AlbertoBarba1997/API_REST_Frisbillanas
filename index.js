ce8bc9a058a1d41379e6db3983f93c973ca79803
AlbertoBarba1997 <juanalbertobarba1997@gmail.com>
2023-10-04 12:45:02 +0200

    03. Script de arranque automatico

---
+++ index.js
@@ -0,0 +1,17 @@
+// Index.js - Carga todo lo que nuestra aplicación va a tener //
+// El proyecto lo vamos a arrancar de manera mas sencilla en un solo paso, ejecutanto el script arrancar_proyecto.bat 
+
+// Conexion a la BBDD
+
+
+
+// Crear servidor node
+
+
+// Configurar cors
+
+
+// Convertir los datos del bodi a objetos js
+
+
+//Poner servidor a escuchar peticiones http
