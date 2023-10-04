8064a9dc2bfc898523d2c8d09cee9bc5bd34746f
AlbertoBarba1997 <juanalbertobarba1997@gmail.com>
2023-10-04 13:48:31 +0200

    05. Poner Servidor en escucha + ruta http de prueba

--- index.js
+++ index.js
@@ -5,20 +5,37 @@ console.log("Aplicacacion Servidor de Frisbillanas arrancada! ");
 
 //Importar dependencias
 const {connection} = require("./database/connection");
-
+const express = require("express");
+const cors = require("cors");
 
 
 // Conexion a la BBDD
 connection();
 
-
 // Crear servidor node
-
+const app = express();
+const puerto = 4200;
 
 // Configurar cors
-
+app.use(cors());
 
 // Convertir los datos del bodi a objetos js
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
+//Cargar conf rutas
+
+
+// (Ruta de prueba)
+app.get("/ruta-prueba" , (req, res)=>{
+    return res.status(200).json({
+        id: 1,
+        nombre : "alberto",
+        apellidos : "barba"
+    })
+})
 
 //Poner servidor a escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("Servidor de node corriendo en el puerto: ", puerto);
+})
